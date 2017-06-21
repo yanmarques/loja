8a64512574d643e6bb1839ad3b0b0d5b0515e1cb
vagrant <vagrant@marques.tecimob.codex>
2017-06-21 17:58:46 +0000

    Modificando o Projeto

--- layout/header.php
+++ layout/header.php
@@ -73,7 +73,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">LOJA YAN</a>
+                <a class="navbar-brand" href="index.php">LOJA TESTE</a>
             </div>
             <div class="collapse navbar-collapse navbar-right" id="myNavbar">
                 <ul class="nav navbar-nav">
